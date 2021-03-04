374c0d996fc3d7c4f168744b67b0294c8a35ce30
serhat güçlü <guclu2349@gmail.com>
2021-03-04 15:58:35 +0300

    10.Gün  çalışma

--- Entities/DTOs/CustomerDetailDto.cs
+++ Entities/DTOs/CustomerDetailDto.cs
@@ -1,12 +1,10 @@
-﻿using Core.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.DTOs
 {
-    public class RentalDetailDto:IDto
+    class CustomerDetailDto
     {
-     
     }
 }
